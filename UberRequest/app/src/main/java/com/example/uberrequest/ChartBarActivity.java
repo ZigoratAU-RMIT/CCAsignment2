e52a03685403c20a3de46040540531ec4b77fc43
Mohammad.Farshidmehr <Mohammad.f@timetabling.com.au>
2020-05-21 00:24:03 +1000

    project configuration

---
+++ UberRequest/app/src/main/java/com/example/uberrequest/ChartBarActivity.java
@@ -0,0 +1,14 @@
+package com.example.uberrequest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChartBarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chart_bar);
+    }
+}
