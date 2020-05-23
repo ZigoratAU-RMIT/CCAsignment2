a539f029748a51fa30cb5edef4b94325e3599438
Mohammad.Farshidmehr <Mohammad.f@timetabling.com.au>
2020-05-23 18:24:22 +1000

    update

---
+++ Cloud Computing Assignment 2/deploy/Client-Android Application/UBERRequest/app/src/main/java/com/example/uberrequest/ChartBarActivity.java
@@ -0,0 +1,66 @@
+package com.example.uberrequest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import lecho.lib.hellocharts.model.Axis;
+import lecho.lib.hellocharts.model.AxisValue;
+import lecho.lib.hellocharts.model.Line;
+import lecho.lib.hellocharts.model.LineChartData;
+import lecho.lib.hellocharts.model.PointValue;
+import lecho.lib.hellocharts.view.LineChartView;
+
+public class ChartBarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chart_bar);
+
+        LineChartView lineChartView = findViewById(R.id.chart);
+        String[] axisData = {"1", "2", "3", "4", "5", "6"};
+        int[] yAxisData = {119203, 89503, 31276, 96884, 25780, 137354};
+        List yAxisValues = new ArrayList();
+        List axisValues = new ArrayList();
+        Line line = new Line(yAxisValues).setColor(Color.parseColor("#9C27B0"));
+        for(int i = 0; i < axisData.length; i++){
+            axisValues.add(i, new AxisValue(i).setLabel(axisData[i]));
+        }
+
+        for (int i = 0; i < yAxisData.length; i++){
+            yAxisValues.add(new PointValue(i, yAxisData[i]));
+        }
+
+        List lines = new ArrayList();
+        lines.add(line);
+
+        LineChartData data = new LineChartData();
+        data.setLines(lines);
+
+        lineChartView.setLineChartData(data);
+
+        Axis xAxis = new Axis();
+        xAxis.setValues(axisValues);
+        data.setAxisXBottom(xAxis);
+
+        Axis yAxis = new Axis();
+        data.setAxisYLeft(yAxis);
+
+        xAxis.setTextSize(16);
+
+        xAxis.setTextColor(Color.parseColor("#03A9F4"));
+
+        yAxis.setTextColor(Color.parseColor("#03A9F4"));
+        yAxis.setTextSize(16);
+
+        yAxis.setName("Sales in 200000");
+        xAxis.setName("Points");
+
+
+    }
+}
